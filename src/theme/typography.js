a530eebaf3eb26304f96f374f696d45dc0967759
Stefano Iachetta <stefano.iachetta@hotmail.it>
2024-01-18 20:40:35 +0100

    refactoring: typography indentation

--- src/theme/typography.js
+++ src/theme/typography.js
@@ -6,8 +6,8 @@ export default {
       lg: {
         'font-size': 70,
         'line-height': 65,
-      }
-    }
+      },
+    },
   },
   displayXL: {
     'font-size': 48,
@@ -16,8 +16,8 @@ export default {
       lg: {
         'font-size': 60,
         'line-height': 72,
-      }
-    }
+      },
+    },
   },
   displayLG: {
     'font-size': 40,
@@ -26,8 +26,8 @@ export default {
       lg: {
         'font-size': 48,
         'line-height': 54,
-      }
-    }
+      },
+    },
   },
   displayMD: {
     'font-size': 32,
@@ -36,8 +36,8 @@ export default {
       lg: {
         'font-size': 40,
         'line-height': 48,
-      }
-    }
+      },
+    },
   },
   displaySM: {
     'font-size': 24,
@@ -46,8 +46,8 @@ export default {
       lg: {
         'font-size': 32,
         'line-height': 40,
-      }
-    }
+      },
+    },
   },
   displayXS: {
     'font-size': 24,
@@ -56,23 +56,23 @@ export default {
       lg: {
         'font-size': 20,
         'line-height': 28,
-      }
-    }
+      },
+    },
   },
-  paragraphSM: {
-    'font-size': 14,
-    'line-height': 24,
-  },
-  paragraphMD: {
-    'font-size': 16,
-    'line-height': 24,
+  paragraphXL: {
+    'font-size': 20,
+    'line-height': 18,
   },
   paragraphLG: {
     'font-size': 18,
     'line-height': 28,
   },
-  paragraphXL: {
-    'font-size': 20,
-    'line-height': 18,
+  paragraphMD: {
+    'font-size': 16,
+    'line-height': 24,
+  },
+  paragraphSM: {
+    'font-size': 14,
+    'line-height': 24,
   },
 }
