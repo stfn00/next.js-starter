4f1562eb9130d1fcb5a97164a56292f0cd2e3bb7
Stefano Iachetta <stefano.iachetta@hotmail.it>
2024-01-18 20:42:27 +0100

    refactor: indentation

--- src/utils/button-aria-props.js
+++ src/utils/button-aria-props.js
@@ -1,21 +1,10 @@
-const getAriaProps = ({
-  type,
-  label,
-  href,
-  target,
-  ariaLabel,
-  rel,
-  disabled,
-  tabIndex
-}) => {
+const getAriaProps = ({ type, label, href, target, ariaLabel, rel, disabled, tabIndex }) => {
   const normalizedRel = (rel || '') + (target === '_blank' ? ' noopener noreferrer' : '')
 
   return {
     'aria-label': ariaLabel || label,
     ...(normalizedRel && { rel: normalizedRel }),
-    ...(disabled
-      ? { 'aria-disabled': true, tabIndex: '-1' }
-      : { tabIndex: tabIndex || '0' }),
+    ...(disabled ? { 'aria-disabled': true, tabIndex: '-1' } : { tabIndex: tabIndex || '0' }),
     ...(!href && { type }),
   }
 }
