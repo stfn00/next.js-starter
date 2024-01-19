74f0ba08dfe3a02b728a0c9825256b7be041d246
Stefano Iachetta <stefano.iachetta@hotmail.it>
2024-01-19 01:17:23 +0100

    chore: add svgr management

--- next.config.js
+++ next.config.js
@@ -6,6 +6,55 @@ const nextConfig = {
     removeConsole: process.env.NODE_ENV !== 'development',
     styledComponents: true,
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            memo: true,
+            dimensions: false,
+            svgoConfig: {
+              multipass: true,
+              plugins: [
+                'removeDimensions',
+                'removeOffCanvasPaths',
+                'reusePaths',
+                'removeElementsByAttr',
+                'removeStyleElement',
+                'removeScriptElement',
+                'removeUselessStrokeAndFill',
+                'prefixIds',
+                'cleanupIds',
+                'convertPathData',
+                {
+                  name: 'cleanupNumericValues',
+                  params: {
+                    floatPrecision: 1,
+                  },
+                },
+                {
+                  name: 'convertTransform',
+                  params: {
+                    floatPrecision: 1,
+                  },
+                },
+                {
+                  name: 'cleanupListOfValues',
+                  params: {
+                    floatPrecision: 1,
+                  },
+                },
+              ],
+            },
+          },
+        },
+      ],
+    })
+
+    return config
+  },
 }
 
 module.exports = nextConfig
