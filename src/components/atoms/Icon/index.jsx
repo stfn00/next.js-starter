acba2239232de75d5267406349a7edd092538136
Stefano Iachetta <stefano.iachetta@hotmail.it>
2024-01-19 01:17:45 +0100

    feat: add Icon based on svgr

--- src/components/atoms/Icon/index.jsx
+++ src/components/atoms/Icon/index.jsx
@@ -1,15 +1,23 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useMemo } from 'react'
+import dynamic from 'next/dynamic'
 
 import * as S from './styles'
 
-const Icon = ({ name, ariaLabel, ...props }) => {
-  const label = ariaLabel ? ariaLabel : name.replaceAll('-', ' ')
+const getIcon = (name) => {
+  const Icon = dynamic(() => import(`@/components/atoms/Icon/glyphs/${name}.svg`), {
+    ssr: false
+  })
+  return Icon
+}
 
-  return (
-    <S.Icon {...props} aria-label={label}>
-      <FontAwesomeIcon icon={name} />
+const Icon = ({ name, ariaLabel, ...props }) => {
+  const SvgIcon = useMemo(() => getIcon(name), [name])
+
+  return SvgIcon ? (
+    <S.Icon {...props} aria-label={ariaLabel || name.replaceAll('-', ' ')}>
+      <SvgIcon />
     </S.Icon>
-  )
+  ) : null
 }
 
 export default Icon
