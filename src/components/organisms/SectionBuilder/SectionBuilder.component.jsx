4fb641509b7d2e97a3bfc1b49e72ccad492cba90
Stefano Iachetta <stefano.iachetta@hotmail.it>
2024-02-02 20:55:42 +0100

    refactor: SectionBuilder rendering

--- src/components/organisms/SectionBuilder/SectionBuilder.component.jsx
+++ src/components/organisms/SectionBuilder/SectionBuilder.component.jsx
@@ -1,5 +1,4 @@
-'use client'
-
+import { memo } from 'react'
 import PropTypes from 'prop-types'
 
 import { SectionThemeContext } from '@/contexts/SectionTheme.context'
@@ -8,29 +7,36 @@ import * as Components from '@/components/organisms'
 
 const componentsDispatcher = {
   // CMS: Organism
+  // Add your component mappings here
 }
 
-const SectionBuilder = ({ data }) =>
-  data &&
-  data.length > 0 &&
+const SectionBuilder = memo(({ data = [] }) =>
   data.map((props, index) => {
     const Component = Components[componentsDispatcher[props.type]]
     const key = `section-${props.type}-${index + 1}`
 
     return Component ? (
-      <SectionThemeContext.Provider key={key} value={props.colorScheme}>
+      <SectionThemeContext.Provider
+        key={key}
+        value={props.colorScheme}
+      >
         <Component {...props} />
       </SectionThemeContext.Provider>
-    ) : null
+    ) : (
+      <div key={key}>Component not found for type: {props.type}</div>
+    )
   })
+)
 
 SectionBuilder.propTypes = {
-  data: PropTypes.arrayOf(PropTypes.shape({
-    type: PropTypes.string.isRequired,
-    sectionID: PropTypes.string,
-    colorScheme: PropTypes.string,
-    // Add additional prop types for each component's props if needed
-  })).isRequired,
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      type: PropTypes.string.isRequired,
+      sectionID: PropTypes.string,
+      colorScheme: PropTypes.string,
+      // Add additional prop types for each component's props if needed
+    })
+  ),
 }
 
 export default SectionBuilder
