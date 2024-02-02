841650b40f8416242606f22bcb8ed6fcb569f8c6
Stefano Iachetta <stefano.iachetta@hotmail.it>
2024-02-02 20:59:27 +0100

    refactor: Typography render optimization

--- src/components/atoms/Typography/Typography.component.jsx
+++ src/components/atoms/Typography/Typography.component.jsx
@@ -1,27 +1,25 @@
+import { memo } from 'react'
 import PropTypes from 'prop-types'
 
 import { useSectionThemeContext } from '@/contexts/SectionTheme.context'
 
 import * as S from './Typography.styles'
 
-const Typography = ({ children, content, ...props }) => {
+const Typography = memo(({ children, content, ...props }) => {
   const colorScheme = useSectionThemeContext()
 
-  const renderContent = () =>
-    typeof children === 'string' ? (
-      <S.Typography
-        {...props}
-        colorScheme={colorScheme}
-        dangerouslySetInnerHTML={{ __html: children }}
-      />
-    ) : (
-      <S.Typography {...props} colorScheme={colorScheme}>
-        {content || children}
-      </S.Typography>
-    )
-
-  return renderContent()
-}
+  return typeof children === 'string' ? (
+    <S.Typography
+      {...props}
+      colorScheme={colorScheme}
+      dangerouslySetInnerHTML={{ __html: children }}
+    />
+  ) : (
+    <S.Typography {...props} colorScheme={colorScheme}>
+      {content || children}
+    </S.Typography>
+  )
+})
 
 Typography.propTypes = {
   children: PropTypes.oneOfType([
