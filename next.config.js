9a7ac27813d1a78146dbaf72790f05cbd6d26cd4
Stefano Iachetta <stefano.iachetta@hotmail.it>
2024-05-28 16:47:32 +0200

    chore: add custom security headers

--- next.config.js
+++ next.config.js
@@ -6,6 +6,45 @@ const nextConfig = {
     removeConsole: process.env.NODE_ENV !== 'development',
     styledComponents: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Origin
+            key: 'Access-Control-Allow-Origin',
+            value: process.env.NEXT_PUBLIC_SITE_URL,
+          },
+          {
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+            key: 'Referrer-Policy',
+            value: 'no-referrer',
+          },
+          {
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-XSS-Protection
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains',
+          },
+        ],
+      },
+    ]
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
