b70af1331e33f7d823a4ad00f6ea74aa0a471a2e
Stefano Iachetta <stefano.iachetta@hotmail.it>
2024-05-31 16:21:28 +0200

    feat: add Icon Story

---
+++ src/stories/atoms/Icons.stories.js
@@ -0,0 +1,50 @@
+import Icon from '@/components/atoms/Icon/Icon.component'
+
+export default {
+  title: 'Atoms/Icons',
+  component: Icon,
+  tags: ['autodocs'],
+  args: {
+    name: 'arrow-right',
+  },
+}
+
+// Variant - Default
+export const Default = {
+  args: {},
+}
+
+// Variant - Small
+export const Small = {
+  args: {
+    size: 'small',
+  },
+}
+
+// Variant - Medium
+export const Medium = {
+  args: {
+    size: 'medium',
+  },
+}
+
+// Variant - Big
+export const Big = {
+  args: {
+    size: 'big',
+  },
+}
+
+// Variant - Huge
+export const Huge = {
+  args: {
+    size: 'huge',
+  },
+}
+
+// Variant - Large
+export const Large = {
+  args: {
+    size: 'large',
+  },
+}
