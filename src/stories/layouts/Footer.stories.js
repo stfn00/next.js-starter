b809159efb8a53b35fff614c3beb7c28774daba5
Stefano Iachetta <stefano.iachetta@hotmail.it>
2024-05-31 17:35:34 +0200

    feat: add Footer Story

---
+++ src/stories/layouts/Footer.stories.js
@@ -0,0 +1,12 @@
+import Footer from '@/components/layouts/Footer/Footer.component'
+
+export default {
+  title: 'Layouts/Footer',
+  component: Footer,
+  tags: ['autodocs'],
+}
+
+// Variant - Default
+export const Default = {
+  args: {}
+}
