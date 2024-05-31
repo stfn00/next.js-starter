7f9331df09a4632e3768f10b1325612b43616479
Stefano Iachetta <stefano.iachetta@hotmail.it>
2024-05-31 17:35:26 +0200

    feat: add Header Story

---
+++ src/stories/layouts/Header.stories.js
@@ -0,0 +1,12 @@
+import Header from '@/components/layouts/Header/Header.component'
+
+export default {
+  title: 'Layouts/Header',
+  component: Header,
+  tags: ['autodocs'],
+}
+
+// Variant - Default
+export const Default = {
+  args: {}
+}
